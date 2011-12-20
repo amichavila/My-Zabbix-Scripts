dfdc2a990f2f69bb28cf4f7bd8c8f5d25c5a73f8
lucho <lucho@hawk.(none)>
2011-12-20 19:05:45 -0200

    -Do not use it yet- This is my second version

--- lucho_zbxapi.py
+++ lucho_zbxapi.py
@@ -22,9 +22,9 @@ def json_req(auth, method, params={}):
 def do_req(myurl, json_req, user, passwd):
 	auth_http='Basic ' + string.strip(base64.encodestring(user + ':' + passwd))
 	headers = { 'Content-Type' : 'application/json-rpc',
-                    'User-Agent'   : 'python/zabbix_api',
+                 'User-Agent'   : 'python/zabbix_api',
 	            'Authorization': auth_http
-                  }
+               }
 	request = urllib2.Request(url=myurl, data=json_req,headers=headers)
 	https_handler = urllib2.HTTPSHandler(debuglevel=0)
 	opener = urllib2.build_opener(https_handler)
@@ -33,6 +33,16 @@ def do_req(myurl, json_req, user, passwd):
 	reads = response.read()
 	jresp = json.loads(reads)
 	return jresp
+
+## This is a simple generator
+def get_results(json_resp):
+	i = 0
+	while True:
+		try:
+			yield json_resp['result'][i]
+			i += 1
+		except IndexError:
+			break
 
 #
 # Main Code
@@ -46,19 +56,18 @@ myurl  = "https://server/zabbix/api_jsonrpc.php"
 req    = json_req(auth=None, method="user.authenticate", params={"user": user, "password": passwd})
 resp   = do_req(myurl, req, user, passwd)
 myauth = resp['result']
+print myauth
 
-# Get all hostgroups
-req    = json_req(auth=myauth, method="hostgroup.get", params={"output": "extended", "sortfield": "name"})
+# Get all hosts in server
+#req    = json_req(auth=myauth, method="host.get", params={"output": "extend"})
+#resp   = do_req(myurl, req, user, passwd)
+#
+#for result in get_results(resp):
+#	print "HostID: {ID} \t Host:{HOST}".format(ID=result['hostid'], HOST=result['host'])
+
+# To this block works properly you must make some changes. Please, read change_api.
+req    = json_req(auth=myauth, method='item.get', params={'output': 'extend', 'filter': {"key_": "vfs.fs.size[%,total]"}})
 resp   = do_req(myurl, req, user, passwd)
-
-groups = resp['result']
-
-for group in groups:
-	print ">> Grupo '{name}' Id: {idg}".format(name=group['name'], idg=group['groupid'])
-	
-	# Get all hosts by groupid
-	req    = json_req(auth=myauth, method="host.get", params={"output": "extended", "filter": {"groupid": group['groupid']}})
-	resp   = do_req(myurl, req, user, passwd)
-	print resp
+print json.dumps(resp, indent=4)
 
 exit()
