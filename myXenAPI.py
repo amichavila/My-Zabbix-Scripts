f38310ba495bd0bebc703abb07e3f1aa9931d500
lucho <lucho@hawk.(none)>
2011-12-21 21:04:48 -0200

    Please don't use it yet

---
+++ myXenAPI.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+#
+# This is my first Xen API.
+# lucho -The black hawk- <luislopez72@gmail.com>
+#
+
+import xmlrpclib
+
+#
+# Functions
+#
+
+def login(xen_server, user, password):
+	login    = xen_server.session.login_with_password(user, password)
+	check_response(response = login)
+	session  = login['Value']
+	return session
+
+def logout(session):
+	xen.session.logout(session)
+
+def check_response(response):
+	if response['Status'] == "Failure":
+		print "An error has ocurred:", login ['ErrorDescription']
+		exit()
+	if not response['Status'] == "Success":
+		print "An error has ocurred: Please report this error..."
+		exit()
+	if not response.has_key('Value'):
+		print "I can't found 'Value' key in the response disctionary..."
+		exit()
+
+def getall_vms(xen_server, session):
+	allvms = xen_server.VM.get_all(session)
+	check_response(response = allvms)
+	return allvms['Value']
+
+def getall_vms_records(xen_server, session):
+	allvms_records = xen_server.VM.get_all_records(session)
+	check_response(response = allvms_records)
+	return allvms_records['Value']
+
+def get_vm_records(xen_server, session, vm):
+	records = xen_server.VM.get_record(session, vm)
+	check_response(response = records)
+	return records['Value']
+
+def countvbds_allvms(xen_server, session, state=None):
+	for vm in  getall_vms(xen_server = xen_server,session = session):
+		fields = get_vm_records(xen_server = xen_server,session = session, vm = vm)
+		if state == "Running":
+			if fields['power_state'] == "Running":
+				print fields['name_label'], fields['VBDs']
+				#print "VM name: {name} \tNumber of vbds: {vbds}".format(name = fields['name_label'], vbds = len(fields['VBDs']))
+		else:
+			print "VM name: {name} \tNumber of vbds: {vbds}".format(name = fields['name_label'], vbds = len(fields['VBDs']))
+
+#
+# Main code
+#
+
+protocol = "https"
+server   = "A.B.C.D"
+port     = "443"
+user     = "root"
+password = "myscretpassword"
+url      = protocol + "://" + server + ":" + port
+xen      = xmlrpclib.Server(url)
+
+#
+# Call your functions here
+#
+session  = login(xen_server = xen, user = user, password = password)
+#get_vm_records(xen_server = xen, session = session, vm = "OpaqueRef:4e32eb31-a733-d8d4-48bc-b7acb77f0e83")
+countvbds_allvms(xen_server = xen, session = session, state = "Running")
+
+## Close your session
+logout(session = session)
+
+exit()
