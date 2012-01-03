0b75be1742b26895303dabe6eba7fcad9a47bb65
lucho <lucho@hawk.(none)>
2012-01-03 16:46:56 -0200

    The first version of this script.

---
+++ scribe.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#############################################
+## This script gather data from scibe servers
+# Created by blackhawk (LuchoLopez)
+# <luislopez72@gmail.com>
+#############################################
+
+#
+# Variables
+#
+
+SCRIBEPORT=1463
+
+
+#
+# Functions
+#
+
+# Print 0 if status="ALIVE", else print 1.
+function get_scribe_status
+{
+   status=`scribe_ctrl status`
+   [[ $status = "ALIVE" ]] && echo 0 || echo -1
+}
+
+# Get the scribe counter for 'scribe_overall:received good:'
+# Print -1 if it not exist.
+function get_overall_good()
+{
+   match=`scribe_ctrl counters $SCRIBEPORT | grep -w ^scribe_overall:received\ good:`
+   [[ $match ]] && echo $match || echo -1
+   unset $match
+}
+
+# Get the scribe counter for 'scribe_overall:received bad:'
+# Print -1 if it not exist.
+function get_overall_bad()
+{
+   match=`scribe_ctrl counters $SCRIBEPORT | grep -w ^scribe_overall:received\ bad:`
+   [[ $match ]] && echo $match || echo -1
+   unset $match
+}
+
